0e655de84df72cd5e4d1f9f65c70ae4c33626f38
bharat-kori <bharatkori.1997@gmail.com.com>
2025-11-25 22:29:16 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='Diamond_price_prediction',
+    version='0.0.1',
+    author='Bharat_kori',
+    author_email='bharatkori.1997@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy']
+)
